e8228273c38b17867936ed4c1a6d83b0259f9057
Manasvi Sharma <manasvi.manu94@gmail.com>
2021-12-01 20:58:45 +0530

    First Portfolio commit

---
+++ src/data/headerData.js
@@ -0,0 +1,12 @@
+import resume from "../assets/pdf/Manasvi_Sharma_Resume.pdf";
+// import { pro_pic } from "./pro_pic";
+
+export const headerData = {
+  name: "Manasvi Sharma",
+  title: "Web Developer",
+  desciption:
+    "There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+  image:
+    "https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png",
+  resumePdf: resume,
+};
